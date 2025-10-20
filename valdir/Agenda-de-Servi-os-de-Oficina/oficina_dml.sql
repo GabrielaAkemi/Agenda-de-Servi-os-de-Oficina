c60d1af1c93c4266051ed9641a197743255e04d9
Gabriela Akemi Rejane Hizukuri Santos <166848865+GabrielaAkemi@users.noreply.github.com>
2025-10-20 20:18:13 -0300

    Refactor SQL inserts and updates for data consistency
    Reinserted data for clients, vehicles, services, and appointments with corrected syntax and structure.

--- valdir/Agenda-de-Servi-os-de-Oficina/oficina_dml.sql
+++ valdir/Agenda-de-Servi-os-de-Oficina/oficina_dml.sql
@@ -1,8 +1,3 @@
-INSERT INTO nomeDaTalea (
-    lista dos campos
-) VALUES (
-    lista dos valores dos campos
-)
 
 INSERT INTO clientes (
     nome,
@@ -10,105 +5,93 @@ INSERT INTO clientes (
     telefone
 ) VALUES (
     "Gabriela Akemi",
-    46891325845,
-    1499806062
-)
+    "46891325845",
+    "1499806062"
+);
 INSERT INTO clientes (
     nome,
     cpf,
     telefone
 ) VALUES (
     "Victor Hugo",
-    54428616856,
-    14997725544
-)
+    "54428616856",
+    "14997725544"
+);
 INSERT INTO clientes (
     nome,
     cpf,
     telefone
 ) VALUES (
     "Ana Julia",
-    25334706806,
-    14981120002
-)
+    "25334706806",
+    "14981120002"
+);
 
 INSERT INTO veiculos (
     id_cliente_ref,
     placa,
     modelo,
-    marca,
-    ano
+    ano,
+    marca
 ) VALUES (
     1,
+    'EII9B12',
     'Creta',
-    'Hyundai',
-    '2021'
-    'EII9B12'
-)
+    '2021-06-07',
+    'Hyundai'
+);
 
 INSERT INTO veiculos (
     id_cliente_ref,
     placa,
     modelo,
-    marca,
-    ano
+    ano,
+    marca
 ) VALUES (
     2,
+    'JID9J13',
     'Ford Ka',
-    'Ford',
-    '2014'
-    'JID9J13'
-)
+    '2014-09-12',
+    'Ford'
+);
 
 INSERT INTO veiculos (
     id_cliente_ref,
     placa,
     modelo,
-    marca,
-    ano
+    ano,
+    marca
 ) VALUES (
     3,
+    'EJB02I9',
     'Hb20',
-    'Hyundai',
-    '2025'
-    'EJB02I9'
-)
+    '2025-05-22',
+    'Hyundai'
+);
 
-INSERT INTO servios (
-    id_cliente_ref,
-    id_veiculo_ref,
+INSERT INTO servicos (
     descricao,
     preco
 ) VALUE(
-    1,
-    1,
     'Troca de Óleo',
     150.00
-)
+);
 
-INSERT INTO servios (
-    id_cliente_ref,
-    id_veiculo_ref,
+INSERT INTO servicos (
     descricao,
     preco
 ) VALUE(
-    2,
-    2,
     'Porta',
     500.00
-)
+);
 
-INSERT INTO servios (
-    id_cliente_ref,
-    id_veiculo_ref,
+INSERT INTO servicos (
     descricao,
     preco
 ) VALUE(
-    3,
-    3,
     'Correia dentada',
     250.00
-)
+);
 
 INSERT INTO agendamentos (
     id_cliente_ref,
@@ -122,7 +105,7 @@ INSERT INTO agendamentos (
     1,
     '2025-10-21 14:00:00', 
     'Em andamento'
-)
+);
 
 INSERT INTO agendamentos (
     id_cliente_ref,
@@ -136,7 +119,7 @@ INSERT INTO agendamentos (
     2,
     '2025-10-22 15:00:00', 
     'Agendado'
-)
+);
 
 INSERT INTO agendamentos (
     id_cliente_ref,
@@ -150,7 +133,7 @@ INSERT INTO agendamentos (
     3,
     '2025-10-23 17:00:00', 
     'Agendado'
-)
+);
 
 SELECT * FROM agendamentos WHERE id_cliente_ref = 1;
 
@@ -171,3 +154,4 @@ DELETE FROM Agendamentos WHERE id_agendamento = 1;
 DELETE FROM Servicos WHERE id_servico = 1;
 
 DELETE FROM Clientes WHERE id_cliente = 1;
+
